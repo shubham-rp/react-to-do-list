8f510b1e60c120d98c2db74ce29fda72abd9a5f5
shubham-rp <shubhamphadte37@gmail.com>
2019-10-18 05:24:14 +0530

    Initial Todo List Commit

---
+++ src/components/Todos.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Todoitem from './Todoitem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+
+  
+
+render(){  
+    
+    return this.props.todos.map((todo)=>
+    <Todoitem key={todo.id} todo={todo} markComplete={this.props.markComplete}
+    delTodo={this.props.delTodo}/>
+    
+    );
+
+}
+}
+
+//PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+}
+
+export default Todos;
